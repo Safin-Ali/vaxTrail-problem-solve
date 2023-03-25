d64ff94684fded62728f53677396d7fed6f6e017
Safin Ali <108370873+Safin-Ali@users.noreply.github.com>
2023-03-25 18:42:06 +0600

    array sort by age

--- vaxTrail-problem-solve.js
+++ vaxTrail-problem-solve.js
@@ -1,30 +1,33 @@
-function vaxTrail(arr = []) {
+const sortArray = (prevArr,current) => {
+    const prevVal = prevArr;
+    return [...prevVal,current].sort((x,y)=>x.age - y.age);
+};
+
+function vaxTrail (arr=[]) {
   const valueObj = {
     A: [],
     B: [],
     C: [],
     D: []
   };
-
+  
   function validation(obj) {
-    if (obj.temperature >= 100) {
-      valueObj.D.push(obj);
-
-    } else if (obj.age > 19 && obj.age < 31 && obj.temperature < 100) {
-      valueObj.A.push(obj)
-        ;
-    } else if (obj.age > 30 && obj.age < 41 && obj.temperature < 100) {
-      valueObj.B.push(obj);
-
-    } else if (obj.age > 40 && obj.age < 51 && obj.temperature < 100) {
-      valueObj.C.push(obj)
+    if(obj.temperature >= 100) { valueObj.D = sortArray(valueObj.D,obj)
+    } else if (obj.age >= 20 && obj.age <= 30 && obj.temperature < 100) {
+      valueObj.A = sortArray(valueObj.A,obj)
+      ;
+    } else if (obj.age >= 31 && obj.age <= 40 && obj.temperature < 100) {
+      valueObj.B = sortArray(valueObj.B,obj)
+      
+    } else if  (obj.age >= 41 && obj.age <= 50 && obj.temperature < 100){
+      sortArray(valueObj.C,obj)
     }
-
+    
   }
-
+  
   arr.map(elm => {
     return validation(elm);
   });
-
+  
   return valueObj
 };
